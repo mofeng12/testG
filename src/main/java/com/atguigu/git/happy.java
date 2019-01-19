dd3e98fd5c7777bdbbb2a7ef33a3dc6b9e7a7c06
tom_glb <goodMorning_glb@atguigu.com>
2019-01-19 19:25:56 +0800

    test conflict task

--- src/main/java/com/atguigu/git/happy.java
+++ src/main/java/com/atguigu/git/happy.java
@@ -2,7 +2,7 @@ package com.atguigu.git;
 
 public class happy {
 	public static void main(String[] args) {
-		System.out.println("test.............");
+		System.out.println("task.............");
 	}
 
 }
