cdb415142de7712a576f73c42d9d3730a7327e5e
tom_glb <goodMorning_glb@atguigu.com>
2019-01-19 19:48:09 +0800

    enit on branch hot_fix

--- src/main/java/com/atguigu/git/happy.java
+++ src/main/java/com/atguigu/git/happy.java
@@ -3,6 +3,7 @@ package com.atguigu.git;
 public class happy {
 	public static void main(String[] args) {
 		System.out.println("task.............");
+		System.out.println("hot_fix.........");
 	}
 
 }
